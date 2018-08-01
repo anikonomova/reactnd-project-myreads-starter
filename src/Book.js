2b12f1968a31f72b98b7f3266f41f763e97cb560
Anna Ikonomova <ani.strandjeva@gmail.com>
2018-08-01 23:08:48 +0200

    Show the books without thumbnail

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ class Book extends Component {
         return(
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : ''})` }}></div>
               <div className="book-shelf-changer">
                 <select onChange={(e)=>{this.props.changeShelf(book,e.target.value);}} defaultValue={book.shelf}>
                   <option value="none" disabled>Move to...</option>
