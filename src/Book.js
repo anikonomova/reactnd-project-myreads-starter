9742372d0961e3e03262de8f3fcaaa24964351f6
Anna Ikonomova <ani.strandjeva@gmail.com>
2018-08-03 21:45:45 +0200

    Fix the 'none' value

--- src/Book.js
+++ src/Book.js
@@ -12,7 +12,7 @@ class Book extends Component {
               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : ''})` }}></div>
               <div className="book-shelf-changer">
                 <select onChange={(e)=>{this.props.changeShelf(book,e.target.value);}} defaultValue={book.shelf}>
-                  <option value="none" disabled>Move to...</option>
+                  <option value="move" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
                   <option value="read">Read</option>
